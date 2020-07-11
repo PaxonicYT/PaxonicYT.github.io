35b8b773e35754344cc5c95c742fddaae232031b
PaxonicYT <47057132+PaxonicYT@users.noreply.github.com>
2020-07-12 00:26:10 +0200

    Update alge.js

--- js/alge.js
+++ js/alge.js
@@ -2,24 +2,18 @@ function farbe() {
     Farben = new Array("Grün","Rot");
     Ergebnis = new Array();
  
-    for(i=0;i<2;i++) {
      Ergebnis.push(Farben[1]);
+
+     document.getElementById('ergebnis') = Ergebnis;
     }
-                         
-    document.getElementById('ergebnis') = Ergebnis;
-                 
- }
 
  function alge() {
     Algen = new Array("Alge 2","Alge 3","Alge 4","Alge 5");
     Ergebnis = new Array();
  
-    for(i=0;i<2;i++) {
      Ergebnis.push(Algen[1]);
-    }
-                         
-    document.getElementById('ergebnis') = Ergebnis;
-                 
+
+     document.getElementById('ergebnis') = Ergebnis;
  }
 
  function iframe_farbe(url) {
